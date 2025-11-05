4241748493c5d7846f3d82ebc3c90462a572c4fe
sophiarodrigues4817 <ser128@Georgetown.edu>
2025-11-05 11:36:50 -0500

    Update rps.py

--- app/rps.py
+++ app/rps.py
@@ -2,7 +2,7 @@ import random
 
 VALID_OPTIONS = ["rock","paper","scissors"]
 
-if__name == "__main__":
+if__name__ == "__main__":
     #only run the code below
     #if we are running this script from teh command line
     #but not if we're trying to just import some stuff from this file 
@@ -41,11 +41,11 @@ def determine_winner(u, c):
     elif u == "rock" and c == "scissors":
         return "USER WINS"
     elif u == "paper" and c == "rock":
-        return "COMPUTER WINS" # OOPS
+        return "USER WINS"
     elif u == "paper" and c == "paper":
         return "TIE GAME"
     elif u == "paper" and c == "scissors":
-        return "USER WINS" # OOPS
+        return "COMPUTER WINS" # OOPS
     elif u == "scissors" and c == "rock":
         return "COMPUTER WINS"
     elif u == "scissors" and c == "paper":
