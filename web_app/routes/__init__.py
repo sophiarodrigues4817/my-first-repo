2d48b353bc268792c4fa74e643368d0241c9bf7d
sophiarodrigues4817 <ser128@Georgetown.edu>
2025-11-17 12:16:13 -0500

    rps game

--- web_app/routes/__init__.py
+++ web_app/routes/__init__.py
@@ -4,6 +4,7 @@
 from flask import Flask
 
 from web_app.routes.home_routes import home_routes
+from web_app.routes.home_routes import home_routes #update
 #from web_app.routes.book_routes import book_routes
 #from web_app.routes.weather_routes import weather_routes
 
