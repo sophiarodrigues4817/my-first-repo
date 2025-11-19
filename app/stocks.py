7c4d019953026f0c7c05ce8c9e584ac12187b052
sophiarodrigues4817 <ser128@Georgetown.edu>
2025-11-19 12:17:00 -0500

    stocks dashboard

--- app/stocks.py
+++ app/stocks.py
@@ -3,8 +3,15 @@ from pandas import read_csv
 
 ALPHAVANTAGE_API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
 
+
+
 symbol = "NFLX"
 request_url = 
 
 stocks_df = read.csv()
 print(stocks_df.head())
+
+def format_usd(price):
+    return f"${price:2f}"
+
+
--- web_app/routes/__init__.py
+++ web_app/routes/__init__.py
@@ -11,6 +11,8 @@ from web_app.routes.home_routes import home_routes #update
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(home_routes)
+    app.register_blueprint(rps_routes)
+    app.register_blueprint(stocks_routes)
     #app.register_blueprint(book_routes)
     #app.register_blueprint(weather_routes)
     return app
@@ -18,5 +20,4 @@ def create_app():
 if __name__ == "__main__":
     my_app = create_app()
     my_app.run(debug=True)
-
     
